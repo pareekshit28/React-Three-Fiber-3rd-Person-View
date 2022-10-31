b5d1ef77347c46479251f23d55be7bae0e7f2e4c
pareekshit28 <pareekshitjoshi28@gmail.com>
2022-10-31 17:32:28 +0530

    Added Walking Controls

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+// import { Model as Plane } from "./components/Plane";
+import { Model as Player } from "./components/Player";
+import { Model as Arcade } from "./components/Arcade";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 0, 0]} />
+      <Suspense fallback={null}>
+        {/* <Plane /> */}
+        <Player scale={[3, 3, 3]} />
+        <Arcade />
+      </Suspense>
+    </Canvas>
   );
 }
 
